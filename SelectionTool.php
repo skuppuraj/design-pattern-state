f8e4e4a0d5b3291b68f32af1325a442a2c70b967
rajkuppu <kuppuraj@live.in>
2021-04-04 21:11:26 +0530

    Add: First SelectionTool class is the Section functionality in photoshop

---
+++ SelectionTool.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * SelectionTool implements Tools Class
+ */
+class SelectionTool extends Tool
+{
+	public function mouseDown(){
+		echo "Selection icon";
+	}
+
+	public function mouseUp(){
+		echo "Draw the dashed rectangle..!";
+	}
+}
